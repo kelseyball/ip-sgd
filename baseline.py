9b8dd951ecb2e41989c13c9582f6f95d933d5bae
Kelsey Ball <kelseytaylorball@gmail.com>
2020-10-18 15:01:48 -0500

    refactor and add inner-product bias
    moved stuff into functions, added an arg parser, and moved hyperparameters into default argument values. added option to sort training set by inner product with current b_hat.
    
    test plan: inspect tensorboard after running baseline.py and baseline.py --ip; b_error decreased *slightly* faster with --ip option

--- baseline.py
+++ baseline.py
@@ -1,3 +1,4 @@
+from argparse import ArgumentParser
 import torch
 from torch.utils.tensorboard import SummaryWriter
 
@@ -7,66 +8,100 @@ from torch.distributions.bernoulli import Bernoulli
 from torch import sigmoid, dot, log
 import random
 
-input_dim = 50
-num_examples = 10000
-step_size = 0.001
-num_epochs = 20
 
-writer = SummaryWriter('runs/baseline')
+def compute_loss(yhat, y):
+    """
+    compute logistic loss function
+    """
+    if (yhat == 0 and y == 0) or (yhat == 1 and y == 1):
+        return 0
+    return -1 * (y * log(yhat) + (1 - y) * log(1 - yhat))
 
-A = torch.rand(input_dim, input_dim)
-covariance_matrix = torch.matmul(A.t(), A)
-m = MultivariateNormal(torch.zeros(input_dim), covariance_matrix)
-b = torch.randn(input_dim)
 
-inputs = [m.sample() for _ in range(num_examples)]
-data = [(x, Bernoulli(sigmoid(dot(b, x))).sample()) for x in inputs]
+def main(args):
+    # fix random seed
+    torch.manual_seed(args.seed)
 
-split_index = int(len(data) * 0.8)
-print(split_index)
-train = data[:split_index]
-val = data[split_index:]
-print(f'{len(train)} train examples, {len(val)} val examples')
+    # set up tensorboard logging
+    label = 'baseline'
+    if args.ip:
+        label = 'inner-product-sgd'
+    writer = SummaryWriter(f'runs/{label}')
 
-bhat = torch.rand(input_dim)
+    # dataset parameters
+    input_dim = 50
+    A = torch.rand(input_dim, input_dim)
+    covariance_matrix = torch.matmul(A.t(), A)
+    m = MultivariateNormal(torch.zeros(input_dim), covariance_matrix)
+    b = torch.randn(input_dim)
 
-yhat_counter = 0
-for epoch in range(num_epochs):
-    random.shuffle(train)
-    print(f"----- epoch {epoch} -----")
-    for i, (x, y) in enumerate(train):
-        yhat = sigmoid(dot(bhat, x))
+    # create dataset
+    inputs = [m.sample() for _ in range(args.num_examples)]
+    data = [(x, Bernoulli(sigmoid(dot(b, x))).sample()) for x in inputs]
 
-        # avoid NaN/inf
-        if yhat == 0 and y == 0:
-            loss = 0
-            yhat_counter += 1
-        elif yhat == 1 and y == 1:
-            loss = 0
-            yhat_counter += 1
+    # train/val split
+    split_index = int(len(data) * 0.8)
+    train = data[:split_index]
+    val = data[split_index:]
+    print(f'{len(train)} train examples, {len(val)} val examples')
+
+    # training initialization
+    bhat = torch.rand(input_dim)
+    step_size = 0.001
+
+    # training loop
+    for epoch in range(args.epochs):
+        if args.ip:
+            train.sort(key=lambda example: dot(example[0], bhat))
         else:
-            loss = -1 * (y * log(yhat) + (1 - y) * log(1 - yhat))
+            random.shuffle(train)
+        print(f"----- epoch {epoch} -----")
+        for i, (x, y) in enumerate(train):
+            # predict and compute loss
+            yhat = sigmoid(dot(bhat, x))
+            loss = compute_loss(yhat, y)
             writer.add_scalar('train_loss', loss, epoch * len(data) + i)
 
-        # compute/update the gradient for each b_i
-        gradient = x * (yhat - y)
-        bhat -= step_size * gradient
+            # compute gradient and update b_hat
+            gradient = x * (yhat - y)
+            bhat -= step_size * gradient
 
-        b_error = np.linalg.norm(bhat - b, 2)
-        writer.add_scalar('b_error', b_error, epoch * len(data) + i)
+            # compute and log || b_hat - b ||
+            b_error = np.linalg.norm(bhat - b, 2)
+            writer.add_scalar('b_error', b_error, epoch * len(data) + i)
 
-    val_loss = 0
-    for i, (x, y) in enumerate(val):
-        yhat = sigmoid(dot(bhat, x))
-        # avoid NaN/inf
-        if (yhat == 0 and y == 0) or (yhat == 1 and y == 1):
-            loss = 0
-        else:
-            loss = -1 * (y * log(yhat) + (1 - y) * log(1 - yhat))
-        val_loss += loss
+        # compute and report avg loss on validation set
+        val_loss = 0
+        for i, (x, y) in enumerate(val):
+            yhat = sigmoid(dot(bhat, x))
+            loss = compute_loss(yhat, y)
+            val_loss += loss
+        val_loss_avg = val_loss / len(val)
+        writer.add_scalar('avg_val_loss', val_loss_avg, epoch)
+        print(f'val loss: {val_loss_avg}')
 
-    val_loss_avg = val_loss / len(val)
-    writer.add_scalar('avg_val_loss', val_loss / len(val), epoch)
-    b_error = np.linalg.norm(bhat - b, 2)
-    writer.add_scalar('b_error', b_error, epoch * len(data) + i)
-    print(f'val loss: {val_loss_avg}')
+
+if __name__ == '__main__':
+    parser = ArgumentParser()
+    parser.add_argument(
+        '--ip',
+        action='store_true',
+        help='if set, bias SGD by inner product <x_i, b>',
+    )
+    parser.add_argument(
+        '--num-epochs', '-e',
+        type=int,
+        default=40,
+    )
+    parser.add_argument(
+        '--num-examples', '-n',
+        type=int,
+        default=10000
+    )
+    parser.add_argument(
+        '--seed', '-s',
+        type=int,
+        default=42
+    )
+    args = parser.parse_args()
+    main(args)
