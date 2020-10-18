e9a9c0daf5d15f1b49e4dd7283cf8c5d7bf05251
Kelsey Ball <kelseytaylorball@gmail.com>
2020-10-18 14:14:23 -0500

    Added validation set and increased dataset size and epochs
    validation loss seems to converge, b_error looks like it could keep decreasing

--- baseline.py
+++ baseline.py
@@ -1,5 +1,4 @@
 import torch
-from math import exp
 from torch.utils.tensorboard import SummaryWriter
 
 import numpy as np
@@ -9,13 +8,12 @@ from torch import sigmoid, dot, log
 import random
 
 input_dim = 50
-num_examples = 1000
+num_examples = 10000
 step_size = 0.001
-num_epochs = 10
+num_epochs = 20
 
 writer = SummaryWriter('runs/baseline')
 
-# TODO: generate random covariance matrix
 A = torch.rand(input_dim, input_dim)
 covariance_matrix = torch.matmul(A.t(), A)
 m = MultivariateNormal(torch.zeros(input_dim), covariance_matrix)
@@ -24,13 +22,19 @@ b = torch.randn(input_dim)
 inputs = [m.sample() for _ in range(num_examples)]
 data = [(x, Bernoulli(sigmoid(dot(b, x))).sample()) for x in inputs]
 
+split_index = int(len(data) * 0.8)
+print(split_index)
+train = data[:split_index]
+val = data[split_index:]
+print(f'{len(train)} train examples, {len(val)} val examples')
+
 bhat = torch.rand(input_dim)
 
 yhat_counter = 0
 for epoch in range(num_epochs):
-    random.shuffle(data)
+    random.shuffle(train)
     print(f"----- epoch {epoch} -----")
-    for i, (x, y) in enumerate(data):
+    for i, (x, y) in enumerate(train):
         yhat = sigmoid(dot(bhat, x))
 
         # avoid NaN/inf
@@ -50,3 +54,19 @@ for epoch in range(num_epochs):
 
         b_error = np.linalg.norm(bhat - b, 2)
         writer.add_scalar('b_error', b_error, epoch * len(data) + i)
+
+    val_loss = 0
+    for i, (x, y) in enumerate(val):
+        yhat = sigmoid(dot(bhat, x))
+        # avoid NaN/inf
+        if (yhat == 0 and y == 0) or (yhat == 1 and y == 1):
+            loss = 0
+        else:
+            loss = -1 * (y * log(yhat) + (1 - y) * log(1 - yhat))
+        val_loss += loss
+
+    val_loss_avg = val_loss / len(val)
+    writer.add_scalar('avg_val_loss', val_loss / len(val), epoch)
+    b_error = np.linalg.norm(bhat - b, 2)
+    writer.add_scalar('b_error', b_error, epoch * len(data) + i)
+    print(f'val loss: {val_loss_avg}')
