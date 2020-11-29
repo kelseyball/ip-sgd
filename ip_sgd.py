b5098ced9129c2521a86d2385932483df4264836
Kelsey Ball <kelseytaylorball@gmail.com>
2020-11-29 14:12:07 -0600

    speed up ip-sgd algorithm
    - replace dumb implementation with less dumb one, plus a unit test
    - fix random seed by default to control for initialization (has more of an impact that i thought)
    - added some more stuff to tensorboard run string
    
    test plan: pytest plus lots of runs (2020-11-28-fixed-LS results)

--- ip_sgd.py
+++ ip_sgd.py
@@ -1,3 +1,4 @@
+from typing import List, Tuple
 from argparse import ArgumentParser
 from tqdm import trange
 import torch
@@ -12,30 +13,59 @@ import random
 from sklearn.metrics import f1_score
 
 
-def sort_negatives(train, bhat):
+def random_positive(train: List[Tuple[torch.tensor, torch.tensor]]):
     """
-    interleave shuffled positives with negatives sorted by <x_i, b_hat>
+    return random positive from dataset
     """
     positives = [e for e in train if e[1].item() == 1.0]
-    negatives = [e for e in train if e[1].item() == 0.0]
-    random.shuffle(positives)
-    negatives.sort(
-        key=lambda example: dot(example[0], bhat),
-        reverse=True,
-    )
-    return [
-        example
-        for pair in zip(positives, negatives)
-        for example in pair
-    ]
+    return random.choice(positives)
 
 
-def logloss(predicted, true_label, eps=1e-15):
+def max_ip_negative(train: List[Tuple[torch.tensor, torch.tensor]], bhat: torch.tensor):
+    """
+    return negative with max inner product with bhat
+    """
+    negatives_with_ip = [(e, dot(e[0], bhat)) for e in train if e[1].item() == 0.0]
+    negative, _ = max(negatives_with_ip, key=lambda x: x[1])
+    return negative
+
+
+def normalize(t: torch.tensor, p=2) -> torch.tensor:
+    """
+    make unit norm w.r.t l-p norm
+    """
+    return t.div(torch.norm(t, p=p))
+
+
+def logloss(predicted, true_label, eps=1e-15) -> float:
     p = np.clip(predicted, eps, 1 - eps)
     if true_label == 1:
         return -np.log(p)
     else:
         return -np.log(1 - p)
+
+
+def generate_data(m: MultivariateNormal, b: torch.tensor, num_examples: int, linearly_separable: bool, positives: float) -> List[Tuple[torch.tensor, torch.tensor]]:
+    """
+    create dataset with given parameters
+    """
+    inputs = [m.sample() for _ in range(num_examples)]
+    data = [(x, Bernoulli(sigmoid(dot(b, x))).sample()) for x in inputs]
+    if linearly_separable:
+        data = [(x, torch.tensor(1.0) if sigmoid(dot(b, x)) > 0.5 else torch.tensor(0)) for x in inputs]
+    if positives < 1.0:
+        data = [(x, y) for (x, y) in data if (y.item() == 0.0) or (y.item() == 1.0 and random.random() < args.positives)]
+    return data
+
+
+def describe_data(data: List[Tuple[torch.tensor, torch.tensor]], writer: SummaryWriter):
+    """
+    log/report count, ratio of positives and negatives
+    """
+    num_positives = sum([1 for e in data if e[1].item() == 1.0])
+    num_negatives = sum([1 for e in data if e[1].item() == 0.0])
+    writer.add_text('num_positives', str(num_positives))
+    writer.add_text('num_negatives', str(num_negatives))
 
 
 def main(args):
@@ -45,43 +75,30 @@ def main(args):
 
     # set up tensorboard logging
     algo = 'ip-sgd' if args.ip else 'vanilla-sgd'
-    label = f'{algo}_n{args.num_examples}_ss{args.step_size}_p{args.positives}'
+    LS = '_LS' if args.ls else ''
+    label = f'{algo}{LS}_n{args.num_examples}_e{args.num_epochs}_ss{args.step_size}_p{args.positives}'
     if args.label:
         label += f'_{args.label}'
     writer = SummaryWriter(f'{args.folder}/{label}')
     writer.add_text('args', str(args))
 
-    # dataset parameters
-    input_dim = 50
-    m = MultivariateNormal(torch.zeros(input_dim), torch.eye(input_dim))
-    b = torch.rand(input_dim)
-
     # create dataset
-    inputs = [m.sample() for _ in range(args.num_examples)]
-    # data = [(x, Bernoulli(sigmoid(dot(b, x))).sample()) for x in inputs]
-
-    # try making data linearly separable
-    data = [(x, torch.tensor(1.0) if sigmoid(dot(b, x)) > 0.5 else torch.tensor(0)) for x in inputs]
-
-    if args.positives < 1.0:
-        # throwaway positives to skew ratio
-        data = [(x, y) for (x, y) in data if (y.item() == 0.0) or (y.item() == 1.0 and random.random() < args.positives)]
-
-    # report count, ratio of positives and negatives
-    num_positives = sum([1 for e in data if e[1].item() == 1.0])
-    num_negatives = sum([1 for e in data if e[1].item() == 0.0])
-    print(f'{num_positives} positives, {num_negatives} negatives')
-    print(f'{num_positives/float(len(data)):.2f}% positives, {num_negatives/float(len(data)):.2f}% negatives')
+    input_dim = args.dim
+    m = MultivariateNormal(torch.zeros(input_dim), torch.eye(input_dim))
+    b = torch.randn(input_dim)
+    data = generate_data(m, b, args.num_examples, args.ls, args.positives)
+    describe_data(data, writer)
 
     # train/val split
     split_index = int(len(data) * 0.8)
     train = data[:split_index]
     val = data[split_index:]
-    print(f'{len(train)} train examples, {len(val)} val examples')
+    writer.add_text('num train examples', str(len(train)))
+    writer.add_text('num val examples', str(len(val)))
     _, y_true_val = tuple(zip(*val))
 
     # training initialization
-    bhat = torch.rand(input_dim)
+    bhat = torch.randn(input_dim)
     step_size = args.step_size
 
     # training loop
@@ -91,8 +108,10 @@ def main(args):
         for i in trange(len(train)):
             x, y = None, None
             if args.ip:
-                train = sort_negatives(train, bhat)
-                (x, y) = random.choice([train[0], train[1]])
+                if random.random() < 0.5:
+                    (x, y) = random_positive(train)
+                else:
+                    (x, y) = max_ip_negative(train, bhat)
             else:
                 (x, y) = train[i]
 
@@ -132,6 +151,12 @@ if __name__ == '__main__':
         help='if set, sort examples by inner product <x_i, b>',
     )
     parser.add_argument(
+        '--dim', '-d',
+        type=int,
+        default=50,
+        help='dimension of data',
+    )
+    parser.add_argument(
         '--num-epochs', '-e',
         type=int,
         default=40,
@@ -144,6 +169,7 @@ if __name__ == '__main__':
     parser.add_argument(
         '--seed', '-r',
         type=int,
+        default=1,
     )
     parser.add_argument(
         '--label', '-l',
@@ -164,6 +190,11 @@ if __name__ == '__main__':
         type=float,
         default=1.0,
         help='percent positives to keep'
+    )
+    parser.add_argument(
+        '--ls',
+        action='store_true',
+        help='if set, make data linearly separable'
     )
     args = parser.parse_args()
     main(args)
--- test_ip_sgd.py
+++ test_ip_sgd.py
@@ -1,8 +1,24 @@
 import pytest
 from pytest import approx
-from ip_sgd import logloss, compute_loss
+from ip_sgd import logloss, normalize, max_ip_negative
 import torch
 
+input_dim = 2
+
+@pytest.fixture
+def bhat():
+    return torch.ones(input_dim)
+
+@pytest.fixture
+def train():
+    return [
+        (torch.full((input_dim,), fill_value=1.0), torch.tensor(1.0)),
+        (torch.full((input_dim,), fill_value=-1.0), torch.tensor(0.0)),
+        (torch.full((input_dim,), fill_value=2.0), torch.tensor(0.0)), # this is the "informative" negative
+        (torch.full((input_dim,), fill_value=-2.0), torch.tensor(0.0)),
+        (torch.full((input_dim,), fill_value=3.0), torch.tensor(1.0)),
+        (torch.full((input_dim,), fill_value=-3.0), torch.tensor(1.0)),
+    ]
 
 def test_logloss_trueneg():
     assert logloss(true_label=0., predicted=0., eps=1e-15) == approx(0, abs=1e-15)
@@ -16,3 +32,17 @@ def test_logloss_truepos():
 def test_logloss_midpt():
     assert logloss(true_label=1., predicted=0.5, eps=1e-15) == approx(.693, abs=1e-3)
     assert logloss(true_label=0., predicted=0.5, eps=1e-15) == approx(.693, abs=1e-3)
+
+def test_normalize_l2():
+    b = torch.randn(50)
+    assert torch.norm(b, p=2).item() != 1.
+    assert torch.norm(normalize(b), p=2).item() == approx(1, abs=1e-5)
+
+def test_normalize_l1():
+    b = torch.randn(50)
+    assert torch.norm(b, p=1).item() != 1.
+    assert torch.norm(normalize(b, p=1), p=1).item() == approx(1, abs=1e-5)
+
+def test_max_ip_negative(train, bhat):
+    max_negative_example = max_ip_negative(train=train, bhat=bhat)
+    assert torch.all(torch.eq(max_negative_example[0], torch.full((input_dim,), fill_value=2.0)))
